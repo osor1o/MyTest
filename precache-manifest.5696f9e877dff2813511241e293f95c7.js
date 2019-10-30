edfc561ad8678405be60b425065442bc06972f44
Osorio Neto <osor1o@hotmail.com>
2019-10-30 06:54:11 -0300

    Updates

---
+++ precache-manifest.5696f9e877dff2813511241e293f95c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe80a8d2c9728ecb1cdc89f865ad231",
+    "url": "/mytest/index.html"
+  },
+  {
+    "revision": "9bfad1cf21a1463e72c5",
+    "url": "/mytest/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9bfad1cf21a1463e72c5",
+    "url": "/mytest/static/js/2.34d93c3d.chunk.js"
+  },
+  {
+    "revision": "2f77b7e3b423d4ff85dd",
+    "url": "/mytest/static/js/main.91b99608.chunk.js"
+  },
+  {
+    "revision": "82d1133fa26a5ea465c0",
+    "url": "/mytest/static/js/runtime-main.6ef29e6c.js"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/mytest/static/media/avatar.9acfe78b.png"
+  }
+]);
